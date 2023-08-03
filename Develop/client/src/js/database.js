3c4bede832e70cd80e1c5a69810a0e84e26a14c6
joeysanchez9 <127254453+joeysanchez9@users.noreply.github.com>
2023-08-03 13:10:42 -0500

    commit

---
+++ Develop/client/src/js/database.js
@@ -0,0 +1,40 @@
+import { openDB } from 'idb';
+
+const initdb = async () =>
+  openDB('jate', 1, {
+    upgrade(db) {
+      if (db.objectStoreNames.contains('jate')) {
+        console.log('jate database already exists');
+        return;
+      }
+      db.createObjectStore('jate', { keyPath: 'id', autoIncrement: true });
+      console.log('jate database created');
+    },
+  });
+
+// TODO: Add logic to a method that accepts some content and adds it to the database
+export const putDb = async (content) => {
+  const db = await inbitdb();
+  const tx = db.transaction('jate', 'readwrite');
+  const store = tx.objectStore('jate');
+  const id = await store.put(content);
+  await tx.done;
+  return id;
+};
+  
+  console.error('putDb not implemented');
+
+// TODO: Add logic for a method that gets all the content from the database
+export const getDb = async () => {
+  const db = await initdb();
+  const tx = db.transaction('jate', 'readonly');
+  const store = tx.objectStore('jate');
+  const content = await store.getAll();
+  await tx.done;
+  return content;
+};
+  
+  
+  console.error('getDb not implemented');
+
+initdb();
---
+++ Develop/client/src/js/install.js
@@ -0,0 +1,32 @@
+const butInstall = document.getElementById('buttonInstall');
+let deferredPrompt;
+
+// Logic for installing the PWA
+// TODO: Add an event handler to the `beforeinstallprompt` event
+window.addEventListener('beforeinstallprompt', (event) => {
+event.preventDefault();
+
+deferredPrompt = event;
+
+butInstall.style.display = 'block';
+});
+// TODO: Implement a click event handler on the `butInstall` element
+butInstall.addEventListener('click', async () => {
+if (deferredPrompt) {
+    deferredPrompt.prompt();
+
+    const result = await deferredPrompt.userChoice;
+    deferredPrompt = null;
+
+    butInstall.style.display = 'none';
+
+    console.log(`User ${result.outcome}`);
+}
+});
+
+// TODO: Add an handler for the `appinstalled` event
+window.addEventListener('appinstalled', (event) => {
+console.log('App installed', event);
+});
+
+
